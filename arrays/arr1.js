632ac6bb7d2fbf934d74f59d76a9ca421df91e53
HarshVardhanK35 <kasa.saiharshavardhan@gmail.com>
2023-11-19 00:12:05 +0530

    Files updated and added

--- arrays/arr1.js
+++ arrays/arr1.js
@@ -1,6 +1,37 @@
-let students = ["John", "Henry"];
+let students = ["John", "Henry", "John"];
 
-let res = students.map((item, index)=>{
-    return (`${item} with ID ${index}`)
-});
-console.log(res);
+// let res = students.map((item, index)=>{
+//     return (`${item} with ID ${index}`)
+// });
+// console.log(res);
+
+// let res = students.filter((item)=>{
+//     if(item[0] == 'J'){
+//         return item;
+//     }
+// })
+// console.log(res);
+
+// let num = [1, 2, 3, 4, 5];
+// let res = num.slice(1, 3);
+// console.log(res)
+
+// function greet(){
+//     return("Hello! This is JS Session");
+// }
+// let clone = greet();
+// let clone1 = greet;
+
+// function details(fName, age = 22){
+//     return ({
+//         firstName: fName,
+//         age: age
+//     })
+// }
+// let person1 = details('John', 23);
+// console.log(person1);
+
+let clone = function sample(){
+    console.log("This is clone function!")
+};
+clone();
